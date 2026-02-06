0d01203ed1e2300e7b1534833db3ad1c9f5631b1
M <marilynduf@gmail.com>
2026-02-05 20:37:21 -0500

    feature: total price of all items in the car displaying

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,6 +1,13 @@
 import Button from "./UI/Button";
 
 export default function CartItem({ ref, onGoToCheckout, meals }) {
+    const totalPrice = meals.reduce(
+        (acc, item) => (acc += item.qty * item.price),
+        0,
+    );
+
+    console.log(totalPrice);
+
     return (
         <dialog ref={ref} className="modal">
             <form method="dialog" className="cart">
@@ -9,7 +16,7 @@ export default function CartItem({ ref, onGoToCheckout, meals }) {
                     {meals.map((meal) => {
                         return (
                             <div className="cart-item">
-                                <p>{meal.name}</p>
+                                <p>{`${meal.name} - ${meal.qty} x $${meal.price}`}</p>
                                 <div className="cart-item-actions">
                                     <button>-</button>
                                     <p>{meal.qty}</p>
@@ -20,7 +27,7 @@ export default function CartItem({ ref, onGoToCheckout, meals }) {
                     })}
                 </ul>
 
-                <p className="cart-total">100</p>
+                <p className="cart-total">{totalPrice}</p>
                 <div className="modal-actions">
                     <Button textOnly>Close</Button>
                     <Button type="button" onClick={onGoToCheckout}>
